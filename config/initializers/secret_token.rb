2422518f56cf2046bfd32dcb3df8183d6c8233e6
Maikel Linke <mkllnk@web.de>
2017-08-05 12:13:01 +1000

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrashI18n::Application.config.secret_token = '5904a5c97a2afb1d18bd6154c4b3eb9cbc59dfc052901ccea8d2f2091267c1ba003bddce77fae640e199d75c65a85aeaebcdc7e7867978c7c2aa321205fd63ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CrashI18n::Application.config.session_store :cookie_store, key: '_crash-i18n_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CrashI18n::Application.config.session_store :active_record_store
